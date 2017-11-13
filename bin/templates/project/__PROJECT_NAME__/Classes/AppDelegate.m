7d1d2787f590f627390442fd93ebfeaf2a742fc2
Walid Owais <walid@squareup.com>
2017-11-13 14:25:41 -0800

    persist cookies when closing and reopening app

--- bin/templates/project/__PROJECT_NAME__/Classes/AppDelegate.m
+++ bin/templates/project/__PROJECT_NAME__/Classes/AppDelegate.m
@@ -30,10 +30,60 @@
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.viewController = [[MainViewController alloc] init];
+    [self loadHTTPCookies];
     return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    [self saveHTTPCookies];
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    [self loadHTTPCookies];
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    [self saveHTTPCookies];
+}
+
+-(void)loadHTTPCookies
+{
+    NSMutableArray* cookieDictionary = [[NSUserDefaults standardUserDefaults] valueForKey:@"cookieArray"];
+
+    for (int i=0; i < cookieDictionary.count; i++)
+    {
+        NSMutableDictionary* cookieDictionary1 = [[NSUserDefaults standardUserDefaults] valueForKey:[cookieDictionary objectAtIndex:i]];
+        NSHTTPCookie *cookie = [NSHTTPCookie cookieWithProperties:cookieDictionary1];
+        [[NSHTTPCookieStorage sharedHTTPCookieStorage] setCookie:cookie];
+    }
+}
+
+-(void)saveHTTPCookies
+{
+    NSMutableArray *cookieArray = [[NSMutableArray alloc] init];
+    for (NSHTTPCookie *cookie in [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookies]) {
+        [cookieArray addObject:cookie.name];
+        NSMutableDictionary *cookieProperties = [NSMutableDictionary dictionary];
+        [cookieProperties setObject:cookie.name forKey:NSHTTPCookieName];
+        [cookieProperties setObject:cookie.value forKey:NSHTTPCookieValue];
+        [cookieProperties setObject:cookie.domain forKey:NSHTTPCookieDomain];
+        [cookieProperties setObject:cookie.path forKey:NSHTTPCookiePath];
+        [cookieProperties setObject:[NSNumber numberWithUnsignedInteger:cookie.version] forKey:NSHTTPCookieVersion];
+        [cookieProperties setObject:[[NSDate date] dateByAddingTimeInterval:2629743] forKey:NSHTTPCookieExpires];
+
+        [[NSUserDefaults standardUserDefaults] setValue:cookieProperties forKey:cookie.name];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+
+    }
+
+    [[NSUserDefaults standardUserDefaults] setValue:cookieArray forKey:@"cookieArray"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
 @end
